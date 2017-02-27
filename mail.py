c922ea0a5a64a15292cf011ec0f23c4339d219d6
WenchenLi <largemaths@gmail.com>
2017-02-27 16:05:46 -0600

    add setup

--- mail.py
+++ mail.py
@@ -6,7 +6,7 @@ from email import MIMEBase
 from email import encoders
 
 
-def send_mail(fromaddr, toaddr, subject, mail_body, send_mail_server,
+def send_mail(fromaddr, toaddr, subject, mail_body, send_mail_server,ps,
               send_mail_port=587, send_file_name_as=None, send_file_path=None):
     """
     simple send mail with body and attachment
@@ -15,6 +15,7 @@ def send_mail(fromaddr, toaddr, subject, mail_body, send_mail_server,
     :param subject: str| subject of email
     :param mail_body: str|email body
     :param send_mail_server: str|
+    :param ps: str| password
     :param send_mail_port: int|
     :param send_file_name_as: str|filename of the attachment
     :param send_file_path: str|path to the attachment
@@ -38,7 +39,7 @@ def send_mail(fromaddr, toaddr, subject, mail_body, send_mail_server,
 
     server = smtplib.SMTP(send_mail_server, send_mail_port)
     server.starttls()
-    server.login(fromaddr, constant.ps)
+    server.login(fromaddr, ps)
     text = msg.as_string()
     server.sendmail(fromaddr, toaddr["To"], text)
     server.quit()
@@ -48,6 +49,6 @@ if __name__=="__main__":
     please define all constants in the constant.py file
     """
     from constant import *
-    send_mail(sender_address, to_address_dict, subject, mail_body, send_mail_server,
+    send_mail(sender_address, to_address_dict, subject, mail_body, send_mail_server,ps,
               send_mail_port=send_mail_port, send_file_name_as=send_file_name_as,
               send_file_path=send_file_path)
