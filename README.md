35d72e559120b3c27499dcca5433004b962d01ba
WenchenLi <largemaths@gmail.com>
2017-02-27 14:37:43 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# mail
+a simple python script for sending email
+  
+# run
+```python
+#please define all constants in the constant.py file
+from constant import *
+
+send_mail(sender_address, to_address_dict, subject, mail_body, send_mail_server,        send_mail_port=send_mail_port, send_file_name_as=send_file_name_as,
+              send_file_path=send_file_path)
+```
---
+++ constant.py
@@ -0,0 +1,15 @@
+sender_address = "sender@example.com"
+ps = "yourpassword!"
+to_address_dict = {"To":["receiver@example.com"],
+                   "CC":["cc@example.com"],
+                   "BCC":["bcc@example.com"]}
+subject = "SUBJECT"
+mail_body = "MAIL BODY\n" \
+            "MAIL BODY\n" \
+            "MAIL BODY\n" \
+            "MAIL BODY\n" \
+            "MAIL BODY\n"
+send_file_name_as = "attechment_file_name"
+send_file_path = '/attachment/file/path'
+send_mail_server = 'mail.example.com'
+send_mail_port = 587
